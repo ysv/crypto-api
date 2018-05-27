88e53fd029f5dec543186e22dfa252aa649b30f2
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-27 16:03:52 +0300

    Better routing

--- v1/main.go
+++ v1/main.go
@@ -3,17 +3,12 @@ package main
 import (
   "log"
   "net/http"
-  "github.com/gorilla/mux"
 )
 
 
 func main() {
 
-  // Init router.
-  router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/", Index).Methods("GET")
-  router.HandleFunc("/currencies", CurrenciesIndex).Methods("GET")
-  router.HandleFunc("/currencies/{code}", CurrencyShow).Methods("GET")
+  router := NewRouter()
 
   // Load currencies.
   LoadCurrencies()
--- v1/routes.go
+++ v1/routes.go
@@ -1,1 +1,22 @@
 package main
+
+var routes = Routes{
+  Route{
+    "Index",
+    "GET",
+    "/",
+    Index,
+  },
+  Route{
+    "TodoIndex",
+    "GET",
+    "/currencies",
+    CurrenciesIndex,
+  },
+  Route{
+    "TodoShow",
+    "GET",
+    "/currencies/{code}",
+    CurrencyShow,
+  },
+}
