b27372d322904d0a0870d9e9579dc6a1a5426a82
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-27 16:26:52 +0300

    Response header & status

--- v1/handlers.go
+++ v1/handlers.go
@@ -6,13 +6,22 @@ import (
   "github.com/gorilla/mux"
 )
 
+func setContentTypeJSON(w http.ResponseWriter){
+  w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+}
+
 func CurrenciesIndex(w http.ResponseWriter, r *http.Request){
+  setContentTypeJSON(w)
+  w.WriteHeader(http.StatusOK)
   json.NewEncoder(w).Encode(currencies)
 }
 
 func CurrencyShow(w http.ResponseWriter, r *http.Request) {
+  setContentTypeJSON(w)
+
   vars := mux.Vars(r)
   currencyId := vars["code"]
+  w.WriteHeader(http.StatusOK)
   json.NewEncoder(w).Encode(CurrencyFindByCode(currencyId))
 }
 
