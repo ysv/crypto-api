8099b6c187b58eefca3fde6dd1368b50fb9d454a
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-29 23:49:01 +0300

    CoinAPIService interface & BTC implementation

--- v1/coin_api_service.go
+++ v1/coin_api_service.go
@@ -5,15 +5,42 @@ import (
   "bytes"
   "fmt"
   "encoding/json"
+  "strings"
+  "strconv"
+  "net/url"
 )
 
-type RPCResponse struct {
+type CoinAPIService interface {
+  rpcCall(method string, optParams ...string) *http.Response
+  getNewAddress() RPCResponseStringRes
+  getBalance()  RPCResponseIntRes
+  sendToAddress(address string, amount int)  RPCResponseStringRes
+}
+
+type BTC struct{
+  Currency
+}
+
+type RPCResponseStringRes struct {
   Result string `json:"result"`
 }
 
-func rpcCall(method string) *http.Response{
-  var jsonStr = []byte(`{"method":"` + method + `"}`)
-  req, _ := http.NewRequest("POST", "http://yaroslav:changeme@127.0.0.1:18332", bytes.NewBuffer(jsonStr))
+type RPCResponseIntRes struct {
+  Result int `json:"result"`
+}
+
+func (btc *BTC) rpcCall(method string, optParams ...string) *http.Response{
+  for i, el := range optParams {
+    optParams[i] = strconv.Quote(el)
+  }
+  optParamsString := strings.Join(optParams, ",")
+  jsonParamsSlice := []string{ `"method":"` + method + `"`}
+  if len(optParamsString) > 0 {
+    jsonParamsSlice = append(jsonParamsSlice, `"params":[` + strings.Join(optParams, ",") + `]`)
+  }
+  var jsonStr = []byte("{" + strings.Join(jsonParamsSlice, ",") + "}")
+  fmt.Println("{" + strings.Join(jsonParamsSlice, ",") + "}")
+  req, _ := http.NewRequest("POST", btc.JSONRPCEndpoint.String(), bytes.NewBuffer(jsonStr))
   client := &http.Client{}
   resp, err := client.Do(req)
   if err != nil {
@@ -25,32 +52,45 @@ func rpcCall(method string) *http.Response{
   return resp
 }
 
-func getNewAddress() RPCResponse{
-  response := rpcCall("getnewaddress")
+func (btc *BTC) getNewAddress() RPCResponseStringRes{
+  response := btc.rpcCall("getnewaddress")
   defer response.Body.Close()
-  rpcResponse := RPCResponse{}
-  //body, _ := ioutil.ReadAll(response.Body)
+  rpcResponse := RPCResponseStringRes{}
   json.NewDecoder(response.Body).Decode(&rpcResponse)
-  //fmt.Println("response Body2:", string(body))
   fmt.Println("genNewAddress: ", rpcResponse.Result)
   return rpcResponse
 }
 
-func getBlockchainInfo()  RPCResponse{
-  response := rpcCall("getbestblockhash")
+func (btc *BTC) getBalance()  RPCResponseIntRes{
+  response := btc.rpcCall("getbalance")
   defer response.Body.Close()
-  rpcResponse := RPCResponse{}
-  //body, _ := ioutil.ReadAll(response.Body)
+  rpcResponse := RPCResponseIntRes{}
   json.NewDecoder(response.Body).Decode(&rpcResponse)
-  //fmt.Println("response Body2:", string(body))
-  fmt.Println("getbestblockhash: ", rpcResponse.Result)
+  fmt.Println("getbalance: ", rpcResponse.Result)
+  return rpcResponse
+}
+
+func (btc *BTC) sendToAddress(address string, amount int)  RPCResponseStringRes{
+  response := btc.rpcCall("sendtoaddress", address, strconv.Itoa(amount))
+  defer response.Body.Close()
+  rpcResponse := RPCResponseStringRes{}
+  json.NewDecoder(response.Body).Decode(&rpcResponse)
+  fmt.Println("sendtoaddress: ", rpcResponse.Result)
   return rpcResponse
 }
 
 func main()  {
-  res, _ := json.Marshal(getNewAddress())
+
+  btcUrl, _ := url.Parse("http://yaroslav:changeme@127.0.0.1:18332")
+  fmt.Println(btcUrl.String())
+  btc := &BTC{ Currency{"BTC", "B", btcUrl}}
+
+  res, _ := json.Marshal(btc.getNewAddress())
   fmt.Println(string(res))
 
-  res, _ = json.Marshal(getBlockchainInfo())
+  res, _ = json.Marshal(btc.getBalance())
+  fmt.Println(string(res))
+
+  res, _ = json.Marshal(btc.sendToAddress("2MtKeBvWttU36TfJARKcPbZgoLQ7KXwB7fT", 10))
   fmt.Println(string(res))
 }
